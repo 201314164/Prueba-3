c43192d9e41fb7cb05722759b5544f4be8c6f959
OSWALDOPC\Oswaldo <gios2911@gmail.com>
2017-06-07 18:42:59 -0600

    Concatenacion

--- source/HojaTrabajo1/HojaTrabajo1/Form1.cs
+++ source/HojaTrabajo1/HojaTrabajo1/Form1.cs
@@ -25,5 +25,15 @@ namespace HojaTrabajo1
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text + textBox2.Text + textBox3.Text;
+        }
     }
 }
